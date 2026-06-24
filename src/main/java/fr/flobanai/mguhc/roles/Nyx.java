ef4a2a0fd63a30cd080fc1a096286a86a5b6c913
Florian SENICOURT <floriansenicourt@gmail.com>
2026-06-24 17:09:54 +0200

    feat(role): added invisibility power to Nyx

--- src/main/java/fr/flobanai/mguhc/roles/Nyx.java
+++ src/main/java/fr/flobanai/mguhc/roles/Nyx.java
@@ -1,16 +1,22 @@
 package fr.flobanai.mguhc.roles;
 
+import java.util.UUID;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import fr.flobanai.mguhc.DataPlayer;
 import fr.flobanai.mguhc.Main;
 
 public class Nyx extends Role implements Listener {
+
+    private UUID playerUuid;
 
     public Nyx() {
         super("Nyx");
@@ -23,6 +29,36 @@ public class Nyx extends Role implements Listener {
 
     @Override
     public void applyBaseStats(DataPlayer player) {
+        this.playerUuid = player.getUuid();
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                Player p = org.bukkit.Bukkit.getPlayer(playerUuid);
+                
+                if (p == null) return; 
+
+                boolean hasNoArmor = true;
+                
+                for (org.bukkit.inventory.ItemStack item : p.getInventory().getArmorContents()){
+                    if (item != null && item.getType() != org.bukkit.Material.AIR) {
+                        hasNoArmor = false;
+                        break;
+                    }
+                }
+                
+                if (hasNoArmor && !Main.isDay){
+                    p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, Integer.MAX_VALUE, 0));
+                }
+                else {
+                    p.removePotionEffect(PotionEffectType.INVISIBILITY);
+                }
+            }
+        }.runTaskTimer(JavaPlugin.getPlugin(Main.class), 40L, 10L);
+    }
+
+    public UUID getPlayerUuid() {
+        return this.playerUuid;
     }
 
     @Override 
@@ -38,17 +74,20 @@ public class Nyx extends Role implements Listener {
     @EventHandler
     public void onPlayerKill(PlayerDeathEvent event){
         Player killer = event.getEntity().getKiller();
-        DataPlayer dp = Main.uhcPlayers.get(killer.getUniqueId());
+        
+        if (killer != null) {
+            DataPlayer dp = Main.uhcPlayers.get(killer.getUniqueId());
 
-        if (dp.getRole().getName().equalsIgnoreCase("Nyx") && !Main.isDay){
-            dp.applyStrength(1);
+            if (dp != null && dp.getRole() != null && dp.getRole().getName().equalsIgnoreCase("Nyx") && !Main.isDay){
+                dp.applyStrength(1);
 
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    dp.applyStrength(0);
-                }
-            }.runTaskLater(JavaPlugin.getPlugin(Main.class), 3600L);
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        dp.applyStrength(0);
+                    }
+                }.runTaskLater(JavaPlugin.getPlugin(Main.class), 3600L);
+            }
         }
     }
 }
